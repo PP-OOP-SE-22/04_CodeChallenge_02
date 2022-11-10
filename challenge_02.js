7a9e52ceb099514ed47b5aa8c4d907adee5b22e4
Sabine Eitel <Seit201@aol.com>
2022-11-10 21:51:54 +0100

    version2.0

--- challenge_02.js
+++ challenge_02.js
@@ -19,32 +19,36 @@ Alter = 13-17 // Cola
 let Name, Age
 Name = prompt("Bitte Name eingeben");
  Age = prompt("Bitte Alter eingeben");
- console.log(Name);
-console.log(Age);
+//  console.log(Name);
+// console.log(Age);
 
-//    Age prompt("Bitte Alter eingeben");
 let Alter1;
-// let Alter2
-// let Alter3
+let Alter2;
+let Alter3;
 
-Alter1 = Number>0, Number<5  // .. Milch  
-// Alter2 = (<12, >6)  // Saft
-// Alter3 = (<17, >13)  // Cola
-
-switch (true) {
-
-    case "Alter1":
-         console.log( Name + "trinkt Milch.");
+Alter1 = Number>0, Number<6  // .. trinkt Milch!  
+Alter2 = Number>5, Number<13  // .. trinkt Saft!
+Alter3 = Number>12, Number<18  //.. trinkt Cola!
+Alter4 = Number>17, Number<100 // .. trinkt Wein!
+switch (false) 
+{
+    case Alter1:
+         console.log (Name + " trinkt Milch!");
     break;
+
+    case Alter2:
+         console.log (Name + " trinkt Saft!");
+    break;
+
+    case Alter3:
+         console.log (Name + " trinkt Cola!");
+     break;
+
+     case Alter4:
+         console.log (Name + " trinkt Wein!");
+     break;
+     
+        default:
+            console.log(Name + " hat keinen Durst sondern Hunger!")
+            break;
 }
-//     // case <12, >6);
-//     //     console.log("Max trinkt Saft");
-//     // break;
-
-//     // case (<17, >13);
-//     //     console.log("Max trinkt Cola");
-
-//         default:
-//             console.log("Max hat keinen Durst, er hat Hunger!")
-//             break;
-
